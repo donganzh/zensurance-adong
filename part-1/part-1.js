de37f22ef121fd7bcd1f64206df0d2a1768d1b91
donganzh <barton97daz@gmail.com>
2022-07-21 01:46:20 -0400

    2 files modified

--- part-1/part-1.js
+++ part-1/part-1.js
@@ -9,18 +9,39 @@ const PROVIDERS = require('../shared/data.json');
  * Create a query that returns total premiums and fees available for retail industry
  */
 function task1() {
-  /**
-   * TODO: Your body goes here
-   */
+  let totalPrem = 0;
+  let totalFees = 0;
+  for (var i = 0; i < PROVIDERS.length; i++) {
+    if (PROVIDERS[i].industry == 'retail') {
+      PROVIDERS[i].prices.forEach(e => {
+        if (e.premium != null && e.premium != '') {
+          totalPrem += parseInt(e.premium);
+        }
+        if (e.fee != null && e.fee != '') {
+          totalFees += parseInt(e.fee);
+        }
+      });
+    }
+  }
+
+  return [{ totalPremium: totalPrem, totalFees: totalFees }];
 }
 
 /**
  * Create a query that returns the minimum premium available among the providers for technology industry
  */
 function task2() {
-  /**
-   * TODO: Your body goes here
-   */
+  let minPrem = Number.MAX_SAFE_INTEGER;
+  for (var i = 0; i < PROVIDERS.length; i++) {
+    if (PROVIDERS[i].industry == 'technology') {
+      PROVIDERS[i].prices.forEach(e => {
+        if (e.premium != null && e.premium != '') {
+          minPrem = Math.min(parseInt(e.premium), minPrem);
+        }
+      });
+    }
+  }
+  return [{ totalPremium: minPrem }];
 }
 
 /**
@@ -31,6 +52,19 @@ function task3() {
   /**
    * TODO: Your body goes here
    */
+  let results = [];
+  for (var i = 0; i < PROVIDERS.length; i++) {
+    if (PROVIDERS[i].industry == 'technology') {
+      PROVIDERS[i].prices.forEach(e => {
+        if (e.premium != null && e.premium != '') {
+          if (parseInt(element.premium) >= 1000) {
+            results.push(PROVIDERS[i].name);
+          }
+        }
+      });
+    }
+  }
+  return results;
 }
 
 /**
--- part-2/part-2.js
+++ part-2/part-2.js
@@ -9,80 +9,102 @@
  * that information if we have to (total price = premium + fee).
  */
 class Price {
-  constructor({ fee, premium }) {}
-
-  add(...prices) {}
-}
-
-/**
- * Right now this function doesn't work because the Price class
- * is not implemented. Your task is to update the class so that
- * the following code produces the correct result as held by the
- * `result` constant.
- *
- * Note that the `.add()` function can take any number of
- * arguments, not just 2 as used here.
- */
-function task1() {
-  const priceA = new Price({
-    premium: 100,
-    fee: 10,
-  });
-  const priceB = new Price({
-    premium: 120,
-    fee: 15,
-  });
-  const priceC = new Price({
-    premium: 80,
-    fee: 20,
-  });
-
-  const result = priceA.add(priceB, priceC);
-
-  // result should have a premium of 300 and a fee of 45
-  return result;
-}
-
-console.log(task1());
-
-/***** TASK 2 *****/
-
-/**
- * Requirements have now changed and we now need to allow prices
- * to be composed of sub prices.
- */
-class NestedPrice extends Price {
+    constructor({ fee, premium }) {
+      this.premium = premium;
+      this.fee = fee;
+    }
+  
+    add(...prices) {
+      prices.forEach((element) => {
+        this.premium += element.premium;
+        this.fee += element.fee;
+      });
+      return { premium: this.premium, fee: this.fee };
+    }
+  }
+  
   /**
-   * The argument `prices` should be an array of instances of
-   * the class Price or NestedPrice.
+   * Right now this function doesn't work because the Price class
+   * is not implemented. Your task is to update the class so that
+   * the following code produces the correct result as held by the
+   * `result` constant.
+   *
+   * Note that the `.add()` function can take any number of
+   * arguments, not just 2 as used here.
    */
-  constructor(prices) {}
-}
-
-/**
- * Your task is to implement `NestedClass` so that the
- * following code also produces the correct results without
- * affecting `Price` (i.e.: both classes need to work on their
- * own).
- */
-function task2() {
-  const priceA = new NestedPrice([
-    new Price({ fee: 5, premium: 50 }),
-    new Price({ fee: 10, premium: 130 }),
-  ]);
-  const priceB = new NestedPrice([
-    new Price({ fee: 10, premium: 70 }),
-    new Price({ fee: 0, premium: 30 }),
-    new NestedPrice([
-      new Price({ fee: 0, premium: 10 }),
-      new Price({ fee: 5, premium: 25 }),
-    ]),
-  ]);
-
-  const result = priceA.add(priceB);
-
-  // result should have a premium of 315 and a fee of 30
-  return result;
-}
+  function task1() {
+    const priceA = new Price({
+      premium: 100,
+      fee: 10,
+    });
+    const priceB = new Price({
+      premium: 120,
+      fee: 15,
+    });
+    const priceC = new Price({
+      premium: 80,
+      fee: 20,
+    });
+  
+    const result = priceA.add(priceB, priceC);
+  
+    // result should have a premium of 300 and a fee of 45
+    return result;
+  }
+  
+  console.log(task1());
+  
+  /***** TASK 2 *****/
+  
+  /**
+   * Requirements have now changed and we now need to allow prices
+   * to be composed of sub prices.
+   */
+  class NestedPrice extends Price {
+    /**
+     * The argument `prices` should be an array of instances of
+     * the class Price or NestedPrice.
+     */
+    constructor(prices) {
+      super(prices);
+      this.premium = 0;
+      this.fee = 0;
+      prices.forEach((elem) => {
+        if (elem.hasOwnProperty('list')) {
+          this.premium += elem.premium;
+          this.fee += elem.fee;
+        } else {
+          this.premium += elem.premium;
+          this.fee += elem.fee;
+        }
+      });
+    }
+  }
+  
+  /**
+   * Your task is to implement `NestedClass` so that the
+   * following code also produces the correct results without
+   * affecting `Price` (i.e.: both classes need to work on their
+   * own).
+   */
+  function task2() {
+    const priceA = new NestedPrice([
+      new Price({ fee: 5, premium: 50 }),
+      new Price({ fee: 10, premium: 130 }),
+    ]);
+    const priceB = new NestedPrice([
+      new Price({ fee: 10, premium: 70 }),
+      new Price({ fee: 0, premium: 30 }),
+      new NestedPrice([
+        new Price({ fee: 0, premium: 10 }),
+        new Price({ fee: 5, premium: 25 }),
+      ]),
+    ]);
+  
+    const result = priceA.add(priceB);
+  
+    // result should have a premium of 315 and a fee of 30
+    return result;
+  }
 
 console.log(task2());
